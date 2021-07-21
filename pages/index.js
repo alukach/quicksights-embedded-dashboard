65ffadd41722b937693fd6e3c70eab01d6c953be
Anthony Lukach <anthonylukach@gmail.com>
2021-07-21 08:34:48 -0600

    Silence warning

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Index() {
   return (<>
     <Head>
       <title>Quicksight Demo</title>
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous"></link>
     </Head>
 
     <nav className="navbar navbar-light bg-light">
